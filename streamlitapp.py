4e24112ae79238380421bb6cf5492bbd5c6b4d75
prajna123 <tupur360@gmail.com>
2025-03-18 16:40:14 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -7,7 +7,7 @@ import os
 API_BASE_URL = os.getenv("API_BASE_URL", "https://diabetes-pred-h2b0apd4g7cbbbcd.northeurope-01.azurewebsites.net")
 
 # Use the correct API endpoint
-FLASK_API_URL = f"{API_BASE_URL}/predict"
+FFLASK_API_URL = "https://diabetes-pred-h2b0apd4g7cbbbcd.northeurope-01.azurewebsites.net/predict"
 
 # Streamlit UI
 st.title("🩺 Diabetes Prediction App")
