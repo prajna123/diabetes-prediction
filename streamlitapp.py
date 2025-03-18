5a5fb1d59251550c71a7edc5c63e9a6906d5175b
prajna123 <tupur360@gmail.com>
2025-03-18 16:46:41 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import requests
 import json
-import os
-
-# Flask API endpoint
-API_BASE_URL = os.getenv("API_BASE_URL", "https://diabetes-pred-h2b0apd4g7cbbbcd.northeurope-01.azurewebsites.net")
 
 # Use the correct API endpoint
-FFLASK_API_URL = "https://diabetes-pred-h2b0apd4g7cbbbcd.northeurope-01.azurewebsites.net/predict"
+FLASK_API_URL = "https://diabetes-pred-h2b0apd4g7cbbbcd.northeurope-01.azurewebsites.net/predict"
 
 # Streamlit UI
 st.title("🩺 Diabetes Prediction App")
